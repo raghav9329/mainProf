32c459102f46cbcb33b2294112e33cd2c727ade4
Mark Atkinson <matkinson2@svb.com>
2016-12-19 11:56:02 -0800

    12/19/16 Capture and addition of current known & similar new files

---
+++ CmdOrScriptFiles/UAT_Entitlements1.sql
@@ -0,0 +1,1 @@
+select * from USER_ENTITLEMENT where USER_ID = 1475803 or USER_ID = 1475804 ; 
---
+++ CmdOrScriptFiles/UAT_Payments.sql
@@ -0,0 +1,3 @@
+Select * from PAYMENT 
+  where CREATOR_ID = 1475803 or CREATOR_ID = 1475804 
+  Order by CREATED_DATE Desc  ;
