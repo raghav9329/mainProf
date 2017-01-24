e0d1aa1c8e96fa5a24b2b3e1643b020f1de315fe
Mark Atkinson <ca60212@deltads.ent>
2017-01-23 18:28:02 -0800

    1/23/17  Got it. the page that is.

--- JsDev/DD_POC_v2/DDCX-SeniorPlan-Single.test.js
+++ JsDev/DD_POC_v2/DDCX-SeniorPlan-Single.test.js
@@ -10,7 +10,7 @@ describe('Delta Dental Senior plan work flow', function() {
 	var testDomain_Name  = "deltadentalins.com";
 	var server = "";
 	var dit3Url = "https://dit3.deltadentalins.com/";
-	var sleepDuration = 1500;
+	var sleepDuration = 500;
 	
 	beforeAll(function () {
 		//var url = protocol.concat(host_mot, '.', testDomain_Name);
@@ -50,79 +50,82 @@ describe('Delta Dental Senior plan work flow', function() {
 		}); //end of it(2)
 	
 		// seems like this should be broken up
-		it('should allow me to enter zip and qty to cover ', function(){
+		it('it 2A: should allow me to enter zip and qty to cover ', function(){
 			expect( element(by.css('.ui-dialog.ui-widget.ui-widget-content.ui-corner-all'))   );
 			
+			/* Waits for the element with id 'coverage_type' to be clickable. 
+			 * */
 			var ExpctCond = protractor.ExpectedConditions;
 
 			var zipValue = '94105';
 			var zipField = element(by.css('.txtFieldC.input-field75.zip.mreq.valid.text.focusme'));	
 			browser.wait(ExpctCond.elementToBeClickable(zipField),5000);
 			zipField.sendKeys(zipValue);
-			
+			console.log('entered date in zipField: ', zipValue);
+		});// end of it(2A) 
 		
-			this.debugTestVar = element(by.css('.txtFieldC.input-field75.zip.mreq.valid.text.focusme'));	
-			//console.log('debugTestVarVaue via Attribute is: ', debugTestVar.getAttribute('value'))
-
-			//this.debugTestVar.getText().then(function (text) {
-			this.debugTestVar.getAttribute('value').then(function (text) {
-				console.log('the zip field text is = ', text);
-			});
-			
+			it('it 2B: special debug step.  should check the text in the field via Attribute',function (){
+				this.debugTestVar = element(by.css('.txtFieldC.input-field75.zip.mreq.valid.text.focusme'));	
+				this.debugTestVar.getAttribute('value').then(function (text) {
+					console.log('debugout: checking text in zip field.  value is = ', text);
+				});
+			}); // end of it(2B)
 		
-			
-			////if (this.debugTestVar.getText(zipValue))
-			//if (this.debugTestVar.getAttribute('value').toEqual('94949'))
-			//	console.log('Woot^3 ',zipValue);
-			//else console.log('fudderupper. We did not get a proper zip code');
-	
-			
-		//	expect(debugTestVar.getText()).toEqual('94105');
-		//	console.log('zip Code Entered and checked to be: ', '94105');
-		
-			
+		it('it 3: should evaluate the single coverage selection', function(){
 			var howManyWillBeCovered = element(by.id('coverage_type'));
 			var coverageSelection = "Self";   //HUMMMM	var coverageSelection = 'Self';  // notice the difference in qoutes
-		
-			/* Waits for the element with id 'coverage_type' to be clickable. 
-			 * */
+			var ExpctCond = protractor.ExpectedConditions; // same name new scope
+
 			browser.wait(ExpctCond.elementToBeClickable(howManyWillBeCovered),5000);
 			howManyWillBeCovered.$('[value="Self"]').click();
-			/* the following is done wrong. HowManyCovered represents a list
-			expect(howManyWillBeCovered.getText()).toEqual('1'); */
+			
+			/* Could not resolve the folllowing checks  */
+			//expect(howManyWillBeCovered.getAttribute('value')).toEqual('1');
+			//expect(howManyWillBeCovered(by.selectedOption('value'))).toEqual('1');
+			//expect(element(by.model('coverage_type')).$('option:checked').getText()).toEqual('1');
+			
 
 			console.log('Single coverage selected with: ',coverageSelection);
-		
+		});// end of it(3)	
 				
-			//it('should find the dialog button and click it', function(){
+			it('should find the dialog button and click it', function(){
 				var searchForPlans_GoButton = element(by.id('btn_saveBig'));	
+				expect( searchForPlans_GoButton.isPresent()).toBe(true);	
 				searchForPlans_GoButton.click();
 				console.log('Search for Dental Plans button found and Button Clicked');
-		//	});
-
-			//expect( 
-			//expect link to Senior is visable
-			// expect I landed on a page with title  ::Individual & Families::.
-			// expect Dental plans for 94105  Var I have !
-			// Expect Link with text USA CAA50 Senior Dental 
+			});	// end of it(4)
+			
+			
+			it('should locate the page titlem properly', function () {
 			var stringPageTitle = "::Individual & Families::";
 			expect(browser.getTitle()).toEqual(stringPageTitle);
 			console.log('Page Title found to be: ',stringPageTitle);
 	
-		
+			});// end of it(5)	
+			
+			it('should find the proper zip code identified for the three plans', function(){	
 			// See that zipValue is defined above some 30 lines 
 			// but do I reall need to do this?  Is it any more readable ?
-			var zipCodeUnderTest = zipValue;
+			var zipCodeUnderTest = '94105';
 			
 			var mySearchCondition = element(by.id('psc_zip'));
-			expect(mySearchCondition.getText()).toEqual(zipValue);
+			expect(mySearchCondition.getText()).toEqual(zipCodeUnderTest);
 			console.log('Found zipCode we are testing', zipCodeUnderTest);
+			});// end of it(6)
+	
+			it('it 7: should find the text of the link for Senior plan' , function(){
+				//var ExpctCond = protractor.ExpectedConditions;
+				var seniorPlanLink = element(by.id('planRowsBody_72230')).element(By.tagName('a'));
+				
+				
+				//browser.wait(ExpctCond.textToBePresentInElementValue)
+				
+				seniorPlanLink.click();
+				console.log('senior plan link found and clicked.')
+			});
 			
-			//browser.pause();	
-
 			
-			
-		});// end of it(3)
+	
 	});// end describe
 
 }); // end of describe('Delta Dental Senior plan work flow
