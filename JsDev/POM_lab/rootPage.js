292520c2103e19e48429cd290117c6533551ecf3
Mark Atkinson <matkinson@delta.org>
2017-03-09 16:48:30 -0800

    3/9/17 capture of POM_Lab stuff that works

--- JsDev/POM_lab/rootPage.js
+++ JsDev/POM_lab/rootPage.js
@@ -9,11 +9,28 @@ module.exports = {
 	go: function() {
 		browser.get('http://dit3.deltadentalins.com/'); // overides baseURL in the spec config
 	},
+
+	setZip_Qty: function (zCode){
+		var EC = protractor.ExpectedConditions;
+		var zipField = element(by.css('.txtFieldC.input-field75.zip.mreq.valid.text.focusme'));
+		var howMany = element(by.id('coverage_type'));	
+		zipField.sendKeys(zCode);
+		browser.wait(EC.elementToBeClickable(howMany),100)
+		howMany.$('[value="Self One"]').click();
+		
+	},
+
+	commit: function(){
+		var commitButton = element(by.id('btn_saveBig'));
+		commitButton.click();
+	},
 	
 	action: function(){
 		var todo = this.toDo;
-		
 		todo.getQuote.click();
-	}
+	},
+	
+
+	
 
 };  // end module.exports
--- JsDev/POM_lab/startGetQuote.js
+++ JsDev/POM_lab/startGetQuote.js
@@ -1,22 +1,25 @@
 'use strict';
 
 var startQuotePage = require('./rootPage.js');
-
+var x;
 describe('Open the browser to the right page & click the getQuote button', function(){
+
 	it('should nave to the starting page', function(){
 		startQuotePage.go();
 	});
 	
 	it('should click the getQuote button',function() {
-	
 		startQuotePage.action();
 		
 		browser.sleep(1800)
+		startQuotePage.setZip_Qty('94949');
+//		startQuotePage.setQty();
+		startQuotePage.commit();
+		browser.sleep(1200)
 	});
 	
 	
-	it('should nave to the starting page again', function(){
-		startQuotePage.go();
-		browser.sleep(8800)
-	});
+	
+
+	
 });
