24fa9fdf47952a93b03b946e09010c490de4f384
Mark Atkinson <matkinson@delta.org>
2017-03-09 17:03:38 -0800

    3/9/17 capture ongoing R&D mods

--- JsDev/DD_POC_v2/protractor-chrome.Jasmine.Gulp.conf.js
+++ JsDev/DD_POC_v2/protractor-chrome.Jasmine.Gulp.conf.js
@@ -4,44 +4,53 @@ const SpecReporter = require('jasmine-spec-reporter')
     Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter')
 
 exports.config = {
-  framework: 'jasmine',
+  //framework: 'jasmine',  3/7/2017 changed this to jasmine2 below
+  framework: 'jasmine2',
   //directConnect: true,
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   //specs: ['DDCX-HP-5Pg2Dp.test.js'],
   //specs: ['./*.test.js'],
-  specs: ['DDCX-SeniorPlan-Single.test.js'],
+  specs: ['integration/scenarios/CAA55PlanSelect.test.js'],
   seleniumServerJar: 'C:\\Users\\CA60212\\AppData\\Roaming\\npm\\node_modules\\protractor\\node_modules\\webdriver-manager\\selenium\\selenium-server-standalone-2.53.1.jar',
   chromeDriver: 'C:\\Users\\CA60212\\AppData\\Roaming\\npm\\node_modules\\protractor\\node_modules\\webdriver-manager\\selenium\\chromedriver_2.26',
   geckodriver:  'C:\\Users\\CA60212\\AppData\\Roaming\\npm\\node_modules\\protractor\\node_modules\\webdriver-manager\\selenium\\geckodriver-v0.12.0',
-  onPrepare: function(){
+   
+  jasmineNodeOpts: {
+        //The default timeout for a spec.  This may need to increase when the amount of tests in a spec increases.
+        defaultTimeoutInterval: 60000,
+    },
+
+
+
+ onPrepare: function(){
    
       /**
        * Configure terminal reporter for protractor status
        */
-      var options = {
-          displayStacktrace: 'none',    // display stacktrace for each failed assertion, values: (all|specs|summary|none)
-          displayFailuresSummary: true, // display summary of all failures after execution
-          displayPendingSummary: true,  // display summary of all pending specs after execution
-          displaySuccessfulSpec: true,  // display each successful spec
-          displayFailedSpec: true,      // display each failed spec
-          displayPendingSpec: true,    // display each pending spec
-          displaySpecDuration: true,   // display each spec duration
-          displaySuiteNumber: true,    // display each suite number (hierarchical)
-          colors: {
-              success: 'green',
-              failure: 'red',
-              pending: 'yellow'
-          },
-          prefixes: {
-              success: '✓ ',
-              failure: '✗ ',
-              pending: '* '
-          }
-      }
+//      var options = {
+//          displayStacktrace: 'none',    // display stacktrace for each failed assertion, values: (all|specs|summary|none)
+//          displayFailuresSummary: true, // display summary of all failures after execution
+//          displayPendingSummary: true,  // display summary of all pending specs after execution
+//          displaySuccessfulSpec: true,  // display each successful spec
+//          displayFailedSpec: true,      // display each failed spec
+//          displayPendingSpec: true,    // display each pending spec
+//          displaySpecDuration: true,   // display each spec duration
+//          displaySuiteNumber: true,    // display each suite number (hierarchical)
+//          colors: {
+//            success: 'green',
+//            failure: 'red',
+//            pending: 'yellow'
+//      },
+//     prefixes: {
+//        success: '✓ ',
+//        failure: '✗ ',
+//        pending: '* '
+//        }
+//   }
       
       // ** Also See http://stackoverflow.com/questions/37173251/how-can-parametrize-protractor-config-file-for-different-browsers-and-test-suite
       // ** for a similar reporter configuration
-      jasmine.getEnv().addReporter(new SpecReporter(options));
+   //   jasmine.getEnv().addReporter(new SpecReporter(options));
 
       /**
        * Configure XML reporter for protractor
