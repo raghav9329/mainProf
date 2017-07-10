ba83c8c6e4314c8a71f763a9aa65340349dc857f
matkinson@delta.org <matkinson@delta.org>
2017-07-10 10:48:18 -0700

    7/10/17 wrap/zip up of hours files. removal duplicate protractor calculator work

---
+++ Bat Proc Killers/XXX___Run___All__Of__These.bat
@@ -0,0 +1,7 @@
+@echo off
+
+call "PsKill ALL Eclipse.bat"
+call "PsKill ALL IE.bat"
+call "PsKill ALL OF Chrome .bat"
+call "PsKill ALL OF FireFox.bat"
+call "PsKill only Test Drivers Chrome.bat"
--- protWork/calculator/conf.js
+++
@@ -1,9 +0,0 @@
-// conf.js
-exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js'],
-  onPrepare: function(){
-   browser.driver.manage().window().setSize(450,650);
-  }
-}
