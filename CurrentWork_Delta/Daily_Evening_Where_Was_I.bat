80b6ab05f8270e0990ab013faded166e822ecbee
Mark Atkinson <matkinson@delta.org>
2018-02-15 11:45:10 -0800

    2/15/18 working out a daily tracking file naming conention, unix2dos, etc. other completed work capture

---
+++ CurrentWork_Delta/Daily_Evening_Where_Was_I.bat
@@ -0,0 +1,15 @@
+@echo off
+:::
+:::
+:::
+:::
+:::
+
+REM  Wrapper to run unix2dos on Daily Evening Where_Was_I.txt
+REM  unix2dos acquired from http://www.efgh.com/software/unix2dos.htm
+REM  after searching https://www.google.com/search?q=win7+unix2dos&rlz=1C1CHBD_enUS756US756&oq=win7+unix&aqs=chrome.2.69i57j0l5.8733j0j7&sourceid=chrome&ie=UTF-8
+REM  There is also unix2dos at https://sourceforge.net/projects/dos2unix/?source=typ_redirect  
+
+c:\bin\unix2dos C:\Mark\CurrentWork_Delta\Daily_Evening_Where_Was_I.txt
+
+call Notepad.exe C:\Mark\CurrentWork_Delta\Daily_Evening_Where_Was_I.txt
